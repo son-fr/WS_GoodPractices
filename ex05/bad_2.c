94f4460221845b319bc64a19e5bdd5530f8c6759
Arthur Jarreau <arthur.jarreau@epitech.eu>
2025-12-18 11:17:27 +0100

    Ex04

---
+++ ex05/bad_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_first_val(int *array)
+{
+    if (array == NULL)
+        return;
+    printf("first = %d\n", array[0]);
+}
+
+int main()
+{
+    int *array = NULL;
+
+    print_first_val(array);
+    return 0;
+}
+
